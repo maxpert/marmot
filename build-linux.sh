949bdfd0cd2f50e85618477b019e73afffd9453b
Zohaib <zohaibsh@gmail.com>
2022-08-26 23:48:01 -0700

    Modify build file

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-GOOS=linux GOARCH=amd64 go build -o marmot-linux-amd64 ./marmot.go
-GOOS=linux GOARCH=386 go build -o marmot-linux-386 ./marmot.go
+GOOS=linux GOARCH=amd64 go build -o build/marmot-linux-amd64 ./marmot.go
+GOOS=linux GOARCH=386 go build -o build/marmot-linux-386 ./marmot.go
