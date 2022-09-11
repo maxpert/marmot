b0a4e5933c661b8c5f2bea383e236e747f105c6f
Zohaib <zohaibsh@gmail.com>
2022-09-11 16:17:39 -0700

    Pebble DB issues scoping down to arm64

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e CGO_ENABLED=1 -e GOARCH=amd64 golang:1.18 go build -v -o build/marmot-linux-amd64 marmot.go
+# docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e CGO_ENABLED=1 -e GOARCH=amd64 golang:1.18 go build -v -o build/marmot-linux-amd64 marmot.go
+
+CC=x86_64-linux-musl-gcc \
+CXX=x86_64-linux-musl-g++ \
+GOARCH=amd64 GOOS=linux CGO_ENABLED=1 \
+go build -ldflags "-linkmode external -extldflags -static" -o dist/marmot-linux-amd64 .
+
